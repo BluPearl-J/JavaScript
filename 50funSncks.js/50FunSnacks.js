7e03354f2e3ef2da65b78b42e545fe18e0a5e030
BluPearl-J <ogheneokeno@gmail.com>
2025-08-11 13:42:14 +0100

    Add files via upload

---
+++ 50funSncks.js/50FunSnacks.js
@@ -0,0 +1,6 @@
+function getName(name){
+const input = nameInput(window.prompt("Type a name",));
+if(typeof(input)==="Alex"){
+console.log("HelloFriend")|}
+elseif(typeof(input)!=="Alex")
+console.log("Hi Stranger")}
